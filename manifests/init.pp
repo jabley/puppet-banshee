9cf89034d5f8a7822f20e69fbf60cda96afd3c0b
James Abley <james.abley@gmail.com>
2013-08-04 09:15:29 +0100

    Create Banshee

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,12 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Banshee to /Applications
+#
+# Sample Usage:
+#
+#  include banshee
+#
+class banshee {
+  package { 'Banshee':
+    provider => 'appdmg',
+    source   => 'http://ftp.gnome.org/pub/GNOME/binaries/mac/banshee/banshee-2.6.1.macosx.intel.dmg'
+  }
 }
